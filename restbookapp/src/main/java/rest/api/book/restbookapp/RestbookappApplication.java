89f7e8fec4d875ae3becde012373a810c2a58d6b
shubhamshinde8 <shubhamshinde8746@gmail.com>
2025-01-26 21:14:13 -1200

    inital commit

---
+++ restbookapp/src/main/java/rest/api/book/restbookapp/RestbookappApplication.java
@@ -0,0 +1,13 @@
+package rest.api.book.restbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbookappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbookappApplication.class, args);
+	}
+
+}
---
+++ restbookapp/src/test/java/rest/api/book/restbookapp/RestbookappApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.api.book.restbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestbookappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
